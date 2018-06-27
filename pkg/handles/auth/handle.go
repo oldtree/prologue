4a2594ab20a7059a0b08270582cc841579f38fd3
suoyanan <suoyanan@sensetime.com>
2018-06-27 20:43:50 +0800

    美式摇滚以及kpop

---
+++ pkg/handles/auth/handle.go
@@ -0,0 +1,14 @@
+package auth
+
+import (
+	"github.com/gin-gonic/gin"
+	log "github.com/sirupsen/logrus"
+)
+
+func init() {
+	log.Infof("init auth handles")
+}
+
+func Handle(g *gin.RouterGroup) {
+
+}
---
+++ pkg/handles/fork/handle.go
@@ -0,0 +1,14 @@
+package fork
+
+import (
+	"github.com/gin-gonic/gin"
+	log "github.com/sirupsen/logrus"
+)
+
+func init() {
+	log.Infof("init fork handles")
+}
+
+func Handle(g *gin.RouterGroup) {
+
+}
